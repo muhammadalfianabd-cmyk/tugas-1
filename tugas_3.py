b0e75c07da221924d29da921414d31e8fffc500b
muhammadalfianabd-cmyk <muhammadalfian.abd@gmail.com>
2025-09-26 20:34:16 +0700

    Create tugas_3.py

---
+++ tugas_3.py
@@ -0,0 +1,6 @@
+nama_depan = str(input("Masukkan nama depan :"))
+nama_belakang = str(input("Masukkan nama belakang :"))
+usia = str(input("Masukkan usia anda :"))
+
+print(f"\nperkenalkan nama saya{nama_depan} {nama_belakang}\nsaya berusia{usia} tahun")
+
