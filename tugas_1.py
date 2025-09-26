2ac9405f5fd5949d22567ab5ab912c93adae1eb5
muhammadalfianabd-cmyk <muhammadalfian.abd@gmail.com>
2025-09-26 20:30:36 +0700

    Create tugas_1.py

---
+++ tugas_1.py
@@ -0,0 +1,7 @@
+nama = input("Masukkan nama lengkap anda: ")
+tahun_lahir = int(input("Masukkan tahun lahir anda: "))
+
+tahun_sekarang = 2025
+umur = tahun_sekarang - tahun_lahir
+
+print(f"Halo, {nama}, umur kamu sekarang {umur} tahun.")
