43a4dffbc59e190c5704596bbc5708a71fab49c6
muhammadalfianabd-cmyk <muhammadalfian.abd@gmail.com>
2025-09-26 20:33:04 +0700

    Create tugas_2.py

---
+++ tugas_2.py
@@ -0,0 +1,5 @@
+nama_lengkap=str(input("masukkan nama buku: "))
+penulis_buku=str(input("masukkan penulis buku: "))
+tahun_terbit=str(input("masukkan tahun terbit: "))
+
+print(f"buku dengan judul {nama_lengkap} ditulis oleh {penulis_buku} dan diterbitkan {tahun_terbit}")
